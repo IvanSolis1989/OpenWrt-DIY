07975f788c0097045a9b8f7d7a01777d2b319008
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2020-09-06 22:54:34 +0800

    Delete Customize.sh

--- config/R2S/ALL/Customize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#1.设置默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' lede/package/base-files/files/bin/config_generate
-#2.修改netgear主题
-cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
-cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css

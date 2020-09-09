0e3fd1d06e0b247f8568279528066b39ee7360d6
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2020-09-09 12:01:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 清除旧版argon主题并拉取最新版
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon

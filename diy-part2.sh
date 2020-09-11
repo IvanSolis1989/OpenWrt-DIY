770d91e207ef7a471f9e0583db7b5c98ca068c2e
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2020-09-11 21:39:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -100,9 +100,3 @@ git clone https://github.com/openwrt-dev/po2lmo.git
 pushd po2lmo
 make && sudo make install
 popd
-
-# Fix libssh
-pushd ../feeds/packages/libs
-rm -rf libssh
-svn co https://github.com/openwrt/packages/trunk/libs/libssh
-popd

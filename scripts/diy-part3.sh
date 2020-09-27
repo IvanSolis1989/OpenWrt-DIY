f455652cab536e992f01868f065e7b247b6f12d4
chouniu666 <71626935+chouniu666@users.noreply.github.com>
2020-09-27 19:31:27 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.10.1/192.168.10.5/g' package/base-files/files/bin/config_generate
 
 # 清除旧版argon主题并拉取最新版
 pushd package/lean

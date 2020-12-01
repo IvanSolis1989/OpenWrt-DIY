5f08278da7cec5a6c33c9535085cd20eab18a1f2
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2020-12-01 09:29:47 +0800

    Update diy-part4.sh

--- scripts/diy-part4.sh
+++ scripts/diy-part4.sh
@@ -105,5 +105,5 @@ popd
 
 sed -i 's/v5.4.18-2020_0402/v5.4.18-2020_0925/g' package/firmware/cypress-firmware/Makefile
 sed -i 's/19375-1-53475/21490-1-58574/g' package/firmware/cypress-firmware/Makefile
-sed -i 's/firmware/brcmfmac/firmware/cyfmac/g' package/firmware/cypress-firmware/Makefile
+sed -i 's/firmware\brcmfmac/firmware\cyfmac/g' package/firmware/cypress-firmware/Makefile
 sed -i 's/b12b0570f462c2f3c26dde98b10235a845a7109037def1e7e51af728bcc1a958/fb71c344e705f5bc9fdae3ce0fbfa299f0af0939ff3ec782aeca0308911d830d/g' package/firmware/cypress-firmware/Makefile

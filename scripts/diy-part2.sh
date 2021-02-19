8c5b599ab90daa836b4db1fade144568605486f8
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2021-02-19 11:55:08 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Clone community packages to package/community
 mkdir package/community

b8d2dd487cd0b8e65f8305cfc081e3909519c3da
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2021-02-23 16:05:27 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -20,6 +20,9 @@ pushd package/community
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
+# Add dnsfilter
+git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter
+
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 

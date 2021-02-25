205797f4c251ed436a4c8e59d888f05f8b161037
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2021-02-25 15:56:29 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -52,7 +52,8 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
-# svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-adguardhome
+svn co https://github.com/immortalwrt/packages/trunk/net/adguardhome
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
@@ -72,7 +73,13 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../lean/luci-theme-argon
 
-# Add luci-theme-argon
+# Use immortalwrt's luci-app-netdata & netdata
+rm -rf ../lean/luci-app-netdata
+rm -rf ../../feeds/packages/admin/netdata
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/luci-app-netdata
+svn co https://github.com/immortalwrt/packages/trunk/admin/netdata
+
+# Add luci-theme-rosy
 git clone --depth=1 -b openwrt-18.06 https://github.com/shiyu1314/luci-theme-rosy
 rm -rf ../lean/luci-theme-rosy
 

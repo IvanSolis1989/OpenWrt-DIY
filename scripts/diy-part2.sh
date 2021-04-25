0587c322cae264edcc7f5b0a22f92b889917739a
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2021-04-25 12:00:26 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -75,9 +75,12 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 # Add luci-app-oled (R2S Only)
 git clone --depth=1 https://github.com/NateLol/luci-app-oled
 
-# Add driver for rtl8821cu & rtl8812au-ac
+# Add extra wireless drivers
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8188eu
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8192du
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl88x2bu
 popd
 
 # Mod zzz-default-settings
@@ -106,3 +109,53 @@ popd
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+# Svn checkout packages from immortalwrt's repository
+pushd customfeeds
+mkdir temp
+git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-18.06 temp/packages
+git clone --depth=1 https://github.com/immortalwrt/luci -b openwrt-18.06 temp/luci
+
+# Add luci-app-adguardhome
+cp -r temp/luci/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
+cp -r temp/packages/net/adguardhome packages/net/adguardhome
+cp -r temp/packages/lang/node-yarn packages/lang/node-yarn
+cp -r temp/packages/devel/packr packages/devel/packr
+
+# Add luci-app-gowebdav
+cp -r temp/luci/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+cp -r temp/packages/net/gowebdav packages/net/gowebdav
+
+# Add netdata
+rm -rf packages/admin/netdata
+rm -rf ../package/lean/luci-app-netdata
+cp -r temp/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
+cp -r temp/packages/admin/netdata packages/admin/netdata
+
+# Add luci-app-smartdns
+cp -r temp/luci/applications/luci-app-smartdns luci/applications/luci-app-smartdns
+cp -r temp/packages/net/smartdns packages/net/smartdns
+
+# Add tmate
+cp -r temp/packages/net/tmate packages/net/tmate
+cp -r temp/packages/libs/msgpack-c packages/libs/msgpack-c
+
+# Add gotop
+cp -r temp/packages/admin/gotop packages/admin/gotop
+
+# Add minieap
+cp -r temp/packages/net/minieap packages/net/minieap
+rm -rf temp
+popd
+
+# Set to local feeds
+pushd customfeeds/packages
+export packages_feed="$(pwd)"
+popd
+pushd customfeeds/luci
+export luci_feed="$(pwd)"
+popd
+sed -i '/src-git packages/d' feeds.conf.default
+echo "src-link packages $packages_feed" >> feeds.conf.default
+sed -i '/src-git luci/d' feeds.conf.default
+echo "src-link luci $luci_feed" >> feeds.conf.default

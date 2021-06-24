c64accc7a55a26c602cd934ff2f4fdd12ccbbde5
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2021-06-24 21:10:50 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -12,6 +12,8 @@
 # Svn checkout packages from immortalwrt's repository
 cd openwrt
 mkdir customfeeds
+git clone --depth=1 https://github.com/coolsnowwolf/packages customfeeds/packages
+git clone --depth=1 https://github.com/coolsnowwolf/luci customfeeds/luci
 pushd customfeeds
 mkdir temp
 git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-18.06 temp/packages

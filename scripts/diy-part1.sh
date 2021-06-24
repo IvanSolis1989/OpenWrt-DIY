94441cdc974b1f010c4ee60bb17a9eac41000e27
IvanSolis1989 <44228691+IvanSolis1989@users.noreply.github.com>
2021-06-24 22:58:30 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -12,7 +12,6 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
